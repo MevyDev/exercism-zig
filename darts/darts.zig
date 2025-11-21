d13d56455833ec24493a02b13aa86ab446fd4c25
MevyDev <mevydev@proton.me>
2025-11-21 01:01:16 +0100

    darts

---
+++ darts/darts.zig
@@ -0,0 +1,21 @@
+pub const Coordinate = struct {
+    x: f32,
+    y: f32,
+
+    pub fn init(x_coord: f32, y_coord: f32) Coordinate {
+        return Coordinate{.x=@abs(x_coord), .y=@abs(y_coord)};
+    }
+    pub fn score(self: Coordinate) usize {
+        const dist: f32 = @sqrt(self.x*self.x + self.y*self.y);
+        if (dist <= 1) {
+            return 10;
+        }
+        if (dist <= 5) {
+            return 5;
+        }
+        if (dist <= 10) {
+            return 1;
+        }
+        return 0;
+    }
+};
